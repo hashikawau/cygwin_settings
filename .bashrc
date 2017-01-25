8e228c21efa6f564b80471e2525f8646a740b584
hashikawa <hashikawa.yuuichi@gmail.com>
2017-01-25 12:34:15 +0900

    Modify merge fail in rebasing

--- .bashrc
+++ .bashrc
@@ -1,9 +1,11 @@
 #----------------------------------------------------------
 # set alias
 #----------------------------------------------------------
+
 if [ -f ~/.bash_aliases ]; then
     . ~/.bash_aliases
 fi
+
 alias ls='ls --color=auto --file-type'
 alias ll='ls -alA'
 alias la='ls -a1A'
@@ -13,4 +15,7 @@ alias fgrep='fgrep --color=auto'
 alias egrep='egrep --color=auto'
 #alias diff='colordiff -u'
 alias wget='wget --restrict-file-names=nocontrol'
+
+alias pbcopy='clip'
+alias pbpaste='cat /dev/clipboard'
 
--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
-<<<<<<< 3719e3eb2513ada94b02cff62553232196f88e67
 #--------------------------------------
 #
 #--------------------------------------
-||||||| merged common ancestors
-=======
-dir_path="`dirname $0`"
-cd ~
-
->>>>>>> temporary commit
 files=(
     .bash_profile
     .bashrc
@@ -29,16 +22,7 @@ cd ~
 for file_name in "${files[@]}"
 do
 (
-<<<<<<< 3719e3eb2513ada94b02cff62553232196f88e67
     target_file_path="${dir_path}/${file_name}"
-||||||| merged common ancestors
-    target_file_path="`pwd`/${file_name}"
-    #echo "${target_file_path}"
-    cd ~
-=======
-    target_file_path="$dir_path/${file_name}"
-    #echo "${target_file_path}"
->>>>>>> temporary commit
     rm -f "${file_name}"
     ln -s "${target_file_path}"
 )
