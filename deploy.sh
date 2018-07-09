88e9c905d09834b1de24b9d8c6c834282e99303c
hashikawa <hashikawa.yuuichi@gmail.com>
2018-07-09 20:27:52 +0900

    Download older git-completion.bash

--- deploy.sh
+++ deploy.sh
@@ -32,7 +32,8 @@ done
 #
 #--------------------------------------
 mkdir -p ~/local/shell
-curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash \
+#curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash \
+curl https://raw.githubusercontent.com/git/git/65ed8ff376c4fdd8dd560b3ddbf7d6cd771f860e/contrib/completion/git-completion.bash \
     > ~/local/shell/git-completion.bash
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh \
     > ~/local/shell/git-prompt.sh
