3312655826a9e6b64bbac880a3ed03da5c567c13
hashikawa <hashikawa.yuuichi@gmail.com>
2019-06-01 18:34:29 +0900

    Modified deploy script

--- deploy.sh
+++ deploy.sh
@@ -15,19 +15,36 @@ files=(
     .vsvimrc
 )
 
-#--------------------------------------
-#
-#--------------------------------------
 cd "`dirname $0`"
 dir_path="`pwd`"
 cd ~
-
 for file_name in "${files[@]}"
 do
 (
     target_file_path="${dir_path}/${file_name}"
+    echo "deploy ${file_name}"
     rm -f "${file_name}"
     ln -s "${target_file_path}"
+    #cp "${target_file_path}" .
+)
+done
+
+#--------------------------------------
+#
+#--------------------------------------
+bin_dirname='local/bin'
+for file_name in "$(ls -1 ${dir_path}/${bin_dirname})"
+do
+(
+    if [ -z ${file_name} ]; then
+        continue
+    fi
+    src_file="${dir_path}/${bin_dirname}/${file_name}"
+    dest_file="${bin_dirname}/${file_name}"
+    echo "deploy ${src_file} -> ${dest_file}"
+    rm -f "${dest_file}"
+    ln -s "${src_file}" "${dest_file}"
+    #cp "${src_file}" "${dest_file}"
 )
 done
 
@@ -35,9 +52,12 @@ done
 #
 #--------------------------------------
 mkdir -p ~/local/shell
-#curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash \
-curl https://raw.githubusercontent.com/git/git/65ed8ff376c4fdd8dd560b3ddbf7d6cd771f860e/contrib/completion/git-completion.bash \
-    > ~/local/shell/git-completion.bash
-curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh \
-    > ~/local/shell/git-prompt.sh
-
+if [ ! -f ~/local/shell/git-completion.bash ]; then
+    #curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash \
+    curl https://raw.githubusercontent.com/git/git/65ed8ff376c4fdd8dd560b3ddbf7d6cd771f860e/contrib/completion/git-completion.bash \
+        > ~/local/shell/git-completion.bash
+fi
+if [ ! -f ~/local/shell/git-prompt.sh ]; then
+    curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh \
+        > ~/local/shell/git-prompt.sh
+fi
